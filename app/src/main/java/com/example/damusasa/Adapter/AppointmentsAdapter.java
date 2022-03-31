44dbc7e2f55d0aac5c5aab9c8026023ade7f3987
joshoxy <joshoxymoron@gmail.com>
2022-03-31 22:48:24 +0300

    Added cancel button to appointments for donors only

--- app/src/main/java/com/example/damusasa/Adapter/AppointmentsAdapter.java
+++ app/src/main/java/com/example/damusasa/Adapter/AppointmentsAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.damusasa.Model.Appointment_model;
 import com.example.damusasa.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -39,6 +46,24 @@ public class AppointmentsAdapter extends RecyclerView.Adapter<AppointmentsAdapte
         holder.centerAddress.setText(user.getCenterAddress());
         holder.Time.setText(user.getTime());
 
+        //Show button if donor is logged in
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String type = snapshot.child("type").getValue().toString();
+                if (type.equals("donor")){
+                    holder.button_cancel.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     @Override
@@ -48,6 +73,7 @@ public class AppointmentsAdapter extends RecyclerView.Adapter<AppointmentsAdapte
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         TextView donorName, centerName, centerAddress, Time;
+        Button button_cancel;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -55,6 +81,7 @@ public class AppointmentsAdapter extends RecyclerView.Adapter<AppointmentsAdapte
             centerName = itemView.findViewById(R.id.all_txt_center_name);
             centerAddress = itemView.findViewById(R.id.all_txt_location);
             Time = itemView.findViewById(R.id.all_txt_time);
+            button_cancel = itemView.findViewById(R.id.button_cancel);
         }
     }
 }
